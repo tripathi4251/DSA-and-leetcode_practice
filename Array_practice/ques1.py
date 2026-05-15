3cc7dec7b7de134810a629fef3b68bbdfc5487a1
tripathi4251 <tripathi.4251@gmail.com>
2026-05-15 14:33:55 +0530

    add basic questions of array

---
+++ Array_practice/ques1.py
@@ -0,0 +1,30 @@
+#Print All Elements of Array
+arry=["apple","banana","chickooo"]
+for x in arry:
+    print(x,end=" ")
+    
+
+#Access the Elements of an Array
+arry=["apple","banana","chickooo"]
+print(arry[0])
+
+
+
+#Add array elements
+arr=[1,2,3,4,5,6]
+arr.append(9)
+print(arr)
+
+
+#remove the spsecific element using index
+arr=["rice","orange","garlic"]
+arr.pop(2)
+print(arr)
+
+
+#remove the element
+arr=["apple","banana","egg","papapya"]
+arr.remove("apple")
+print(arr)
+
+
